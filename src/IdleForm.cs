5f36f17cd0894679d0dc600ec071216ea7f29f8c
Dzmitri Umetsky <sedrpdasavushkinby@gmail.com>
2020-02-24 11:33:18 +0300

    Creating idle application module (#142)
    * [fix] Creating idle module classes and added pinvoke getlastuserinfo
    
    * [fix] Added GetLatInputTime method
    
    * [fix] Created timer form
    
    * [fix] Work on timers
    
    * [fix] Work on new functionality
    
    * [fix] Closing eplan after time is over
    
    * [fix] Corrects
    
    * [fix] Correct methods
    
    * [fix] Fixes
    
    * [fix] Implementing process.kill()

---
+++ src/IdleForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EasyEPlanner
+{
+    public partial class IdleForm : Form
+    {
+        private IdleForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Инициализация таймера.
+        /// </summary>
+        public void RunCountdown()
+        {
+            timerLabel.Text = $"Осталось: {startingCountdown} с.";
+            RunTimer();
+        }
+
+        /// <summary>
+        /// Запуск отсчета времени и запуск формы.
+        /// </summary>
+        private void RunTimer()
+        {
+            countdownTimer = new Timer();
+            countdownTimer.Interval = countdownInterval;
+            countdownTimer.Tick += new EventHandler(TimerWorking);
+            countdownTimer.Start();
+
+            Application.Run(this);
+        }
+
+        /// <summary>
+        /// Остановить таймер.
+        /// </summary>
+        private void StopCountdown()
+        {
+            countdownTimer.Stop();
+            countdownTimer.Tick -= new EventHandler(TimerWorking);
+        }
+
+        /// <summary>
+        /// Работа таймера.
+        /// </summary>
+        /// <param name="sencder"></param>
+        /// <param name="e"></param>
+        private void TimerWorking(object sencder, EventArgs e)
+        {
+            startingCountdown--;
+            if (startingCountdown > 0)
+            {
+                timerLabel.Text = $"Осталось: {startingCountdown} с.";
+            }
+            else
+            {
+                this.Hide();
+                this.Close();
+                StopCountdown();
+                IdleModule.Stop();
+                IdleModule.CloseApplication();
+            }
+        }
+
+        /// <summary>
+        /// Подтверждение активности.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+            StopCountdown();
+            this.Hide();
+            this.Close();
+        }
+
+        /// <summary>
+        /// Получить форму.
+        /// </summary>
+        public static IdleForm Form
+        {
+            get
+            {
+                if (idleForm == null)
+                {
+                    idleForm = new IdleForm();
+                }
+                if (idleForm.IsDisposed == true)
+                {
+                    idleForm = new IdleForm();
+                }
+
+                return idleForm;
+            }
+        }
+
+        /// <summary>
+        /// Таймер.
+        /// </summary>
+        private Timer countdownTimer;
+
+        /// <summary>
+        /// Стартовое значение таймера.
+        /// </summary>
+        private int startingCountdown = 60;
+
+        /// <summary>
+        /// Интервал опроса таймера в миллисекундах.
+        /// </summary>
+        private const int countdownInterval = 1000;
+
+        /// <summary>
+        /// Форма с таймером.
+        /// </summary>
+        private static IdleForm idleForm = null;
+    }
+}
---
+++ src/IdleModule.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using PInvoke;
+using System.Threading;
+using System.Diagnostics;
+
+namespace EasyEPlanner
+{
+    /// <summary>
+    /// Класс, отвечающий за модуль простоя приложения
+    /// </summary>
+    public static class IdleModule
+    {
+        /// <summary>
+        /// Запустить поток модуля простоя приложения
+        /// </summary>
+        public static void Start()
+        {
+            idleThread = new Thread(Run);
+            idleThread.Start();
+        }
+
+        /// <summary>
+        /// Остановить модуль простоя приложения
+        /// </summary>
+        public static void Stop()
+        {
+            isRunning = false;
+        }
+
+        /// <summary>
+        /// Закрыть приложение.
+        /// </summary>
+        public static void CloseApplication()
+        {
+            Process eplanProcess = Process.GetCurrentProcess();
+            var isClosed = eplanProcess.CloseMainWindow();
+            if (isClosed == false)
+            {
+                var project = EProjectManager.GetInstance().GetCurrentPrj();
+                if (project != null)
+                {
+                    EProjectManager.GetInstance().SyncAndSave();
+                    Thread.Sleep(500);
+                    project.Close();
+                }
+                Thread.Sleep(500);
+                eplanProcess.Kill();          
+            }
+            else
+            {
+                eplanProcess.Close();
+            }
+        }
+
+        /// <summary>
+        /// Запустить модуль
+        /// </summary>
+        private static void Run()
+        {
+            isRunning = true;
+            while (isRunning)
+            {
+                CheckIdle();
+                Thread.Sleep(idleInterval);
+            }
+        }
+
+        /// <summary>
+        /// Проверка состояния простоя
+        /// </summary>
+        private static void CheckIdle()
+        {
+            if (GetLastInputTime() > MaxIdleTime)
+            {
+                ShowCountdownWindow();
+            }
+        }
+
+        /// <summary>
+        /// Показать форму с таймером и запустить таймер
+        /// </summary>
+        private static void ShowCountdownWindow()
+        {
+            IdleForm.Form.Show();
+            IdleForm.Form.RunCountdown();
+        }
+
+        /// <summary>
+        /// Получить время последнего ввода пользователя
+        /// </summary>
+        /// <returns>Время в миллисекундах</returns>
+        private static uint GetLastInputTime()
+        {
+            uint idleTime = 0;
+            PI.LASTINPUTINFO lastInputInfo = new PI.LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            uint envTicks = (uint)Environment.TickCount;
+
+            if (PI.GetLastInputInfo(ref lastInputInfo))
+            {
+                uint lastInputTick = lastInputInfo.dwTime;
+
+                idleTime = envTicks - lastInputTick;
+            }
+
+            return ((idleTime > 0) ? idleTime : 0);
+        }
+
+        /// <summary>
+        /// Максимальное время простоя в миллисекундах
+        /// </summary>
+        private const uint MaxIdleTime = 6000/*3600000*/;
+
+        /// <summary>
+        /// Интервал проверки простоя в миллисекундах
+        /// </summary>
+        private const int idleInterval = 1000/*60000*/;
+
+        /// <summary>
+        /// Флаг запуска потока.
+        /// </summary>
+        private static bool isRunning = true;
+
+        /// <summary>
+        /// Поток модуля простоя
+        /// </summary>
+        private static Thread idleThread;
+    }
+}
