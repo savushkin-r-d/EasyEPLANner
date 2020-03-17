422549a4e068ac0b511d007870688506a717e586
dima_beel <sedrpdasavushkinby@gmail.com>
2020-03-17 14:35:36 +0300

    [fix] Implemented cooling base operation

--- src/TechObject/Base/TechObjects/BaseWaterTank.cs
+++ src/TechObject/Base/TechObjects/BaseWaterTank.cs
@@ -35,5 +35,58 @@ namespace TechObject
             cloned.Owner = techObject;
             return cloned;
         }
+
+        #region сохранение prg.lua
+        /// <summary>
+        /// Сохранить информацию об операциях объекта в prg.lua
+        /// </summary>
+        /// <param name="objName">Имя объекта для записи</param>
+        /// <param name="prefix">Отступ</param>
+        /// <returns></returns>
+        public override string SaveOperationsToPrgLua(string objName,
+            string prefix)
+        {
+            var res = "";
+
+            var modesManager = this.Owner.ModesManager;
+            var modes = modesManager.Modes;
+            foreach (Mode mode in modes)
+            {
+                var baseOperation = mode.GetBaseOperation();
+                switch (baseOperation.Name)
+                {
+                    case "Охлаждение":
+                        res += SaveCoolingOperation(prefix, objName, mode,
+                            baseOperation);
+                        break;
+                }
+            }
+
+            res += "\n";
+            return res;
+        }
+
+        /// <summary>
+        /// Сохранить операцию охлаждения.
+        /// </summary>
+        /// <param name="prefix">Отступ</param>
+        /// <param name="objName">Имя объекта</param>
+        /// <param name="mode">Операция</param>
+        /// <param name="baseOperation">Базовая операция</param>
+        /// <returns></returns>
+        private string SaveCoolingOperation(string prefix, string objName,
+            Mode mode, BaseOperation baseOperation)
+        {
+            var res = "";
+
+            res += objName + ".operations = \t\t--Операции.\n";
+            res += prefix + "{\n";
+            res += prefix + baseOperation.LuaName.ToUpper() + " = " +
+                mode.GetModeNumber() + ",\t\t--Охлаждение.\n";
+            res += prefix + "}\n";
+
+            return res;
+        }
+        #endregion
     }
 }
