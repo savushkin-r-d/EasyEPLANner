35617285af27d591f22f79546fca649d617713f7
dima_beel <sedrpdasavushkinby@gmail.com>
2020-03-17 10:11:35 +0300

    [fix] Refactoring

--- src/Lua/sys.lua
+++ src/Lua/sys.lua
@@ -14,8 +14,8 @@ init = function()
 		local attached_objects	  = value.attached_objects or ""
 
         local obj = ADD_TECH_OBJECT( object_n, object_name, object_tech_type,
-            object_name_eplan, cooper_param_number, object_name_BC, base_tech_object,
-			attached_objects)
+            object_name_eplan, cooper_param_number, object_name_BC, 
+            base_tech_object, attached_objects)
 
         local timers_count = value.timers or 1
         obj:SetTimersCount( timers_count )
@@ -47,7 +47,8 @@ init = function()
 				end
 			end
 
-            local mode      = obj:AddMode( mode_name, mode_base_operation, mode_base_operation_props)
+            local mode = obj:AddMode( mode_name, mode_base_operation, 
+            mode_base_operation_props)
 
             local idx = fields
             proc_oper_params( params_float, mode, idx, obj )
@@ -72,103 +73,43 @@ end
 --Обработка сохраненного описания операции.
 proc_operation = function( value, mode, state_n )
     proc( mode, state_n, value.opened_devices, -1, "opened_devices" )
-    proc( mode, state_n, value.opened_reverse_devices, -1, "opened_reverse_devices" )
+    proc( mode, state_n, value.opened_reverse_devices, -1, 
+    "opened_reverse_devices" )
     proc( mode, state_n, value.closed_devices, -1, "closed_devices" )
 
-    proc_seats( mode, state_n, -1, value.opened_upper_seat_v, "opened_upper_seat_v" )
-    proc_seats( mode, state_n, -1, value.opened_lower_seat_v, "opened_lower_seat_v" )
+    proc_seats( mode, state_n, -1, value.opened_upper_seat_v, 
+    "opened_upper_seat_v" )
+    proc_seats( mode, state_n, -1, value.opened_lower_seat_v, 
+    "opened_lower_seat_v" )
+    
     proc( mode, state_n, value.required_FB,    -1, "required_FB" )
 
     proc_seats(mode, state_n, -1, value.DI_DO, "DI_DO")
     proc_seats(mode, state_n, -1, value.AI_AO, "AI_AO")
 
-    --Группа устройств, управляемых по ОС с выдачей сигнала.
-    if value.wash_data ~= nil then
-
-        --DI
-        if value.wash_data.DI ~= nil then
-            mode[ state_n ][ -1 ]:AddDev( "wash_data", value.wash_data.DI[ 1 ], 0 )
-        end
-
-        --Control signal DO
-        if value.wash_data.DO ~= nil then
-            for field, value in pairs( value.wash_data.DO ) do
-                mode[ state_n ][ -1 ]:AddDev( "wash_data", value, 1 )
-            end
-        end
-
-        --On devices.
-        if value.wash_data.devices ~= nil then
-            for field, value in pairs( value.wash_data.devices ) do
-                mode[ state_n ][ -1 ]:AddDev( "wash_data", value, 2 )
-            end
-        end
-
-        --On reverse devices.
-        if value.wash_data.rev_devices ~= nil then
-            for field, value in pairs( value.wash_data.rev_devices ) do
-                mode[ state_n ][ -1 ]:AddDev( "wash_data", value, 3 )
-            end
-        end
-
-        --Frequency param.
-        if value.wash_data.pump_freq ~= nil then
-            mode[ state_n ][ -1 ]:AddParam( "wash_data", 1,
-                value.wash_data.pump_freq )
-        end
-    end -- if value.wash_data ~= nil then
+    proc_wash_data(mode, state_n, -1, value)
 
     if value.steps ~= nil then
         for fields, value in ipairs( value.steps ) do
             mode:AddStep( state_n, value.name or "Шаг ??" )
             local step_n = fields - 1
 
-            proc( mode, state_n, value.opened_devices, step_n, "opened_devices" )
-            proc( mode, state_n, value.opened_reverse_devices, step_n, "opened_reverse_devices" )
-            proc( mode, state_n, value.closed_devices, step_n, "closed_devices" )
+            proc( mode, state_n, value.opened_devices, step_n, 
+            "opened_devices" )
+            proc( mode, state_n, value.opened_reverse_devices, step_n, 
+            "opened_reverse_devices" )
+            proc( mode, state_n, value.closed_devices, step_n, 
+            "closed_devices" )
             proc_seats( mode, state_n, step_n, value.opened_upper_seat_v,
-                "opened_upper_seat_v" )
+            "opened_upper_seat_v" )
             proc_seats( mode, state_n, step_n, value.opened_lower_seat_v,
-                "opened_lower_seat_v" )
+            "opened_lower_seat_v" )
             proc( mode, state_n, value.required_FB, step_n, "required_FB" )
 
             proc_seats(mode, state_n, step_n, value.DI_DO, "DI_DO")
             proc_seats(mode, state_n, step_n, value.AI_AO, "AI_AO")
 
-            --Группа устройств, управляемых по ОС с выдачей сигнала.
-            if value.wash_data ~= nil then
-                --DI
-                if value.wash_data.DI ~= nil then
-                    mode[ state_n ][ step_n ]:AddDev( "wash_data", value.wash_data.DI[ 1 ], 0 )
-                end
-
-                --Control signal DO
-                if value.wash_data.DO ~= nil then
-                    for field, value in pairs( value.wash_data.DO ) do
-                        mode[ state_n ][ step_n ]:AddDev( "wash_data", value, 1 )
-                    end
-                end
-
-                --On devices.
-                if value.wash_data.devices ~= nil then
-                    for field, value in pairs( value.wash_data.devices ) do
-                        mode[ state_n ][ step_n ]:AddDev( "wash_data", value, 2 )
-                    end
-                end
-
-                --On reverse devices.
-                if value.wash_data.rev_devices ~= nil then
-                    for field, value in pairs( value.wash_data.rev_devices ) do
-                        mode[ state_n ][ step_n ]:AddDev( "wash_data", value, 3 )
-                    end
-                end
-
-                --Frequency param.
-                if value.wash_data.pump_freq ~= nil then
-                    mode[ state_n ][ step_n ]:AddParam( "wash_data", 1,
-                        value.wash_data.pump_freq )
-                end
-            end -- if value.wash_data ~= nil then
+            proc_wash_data(mode, state_n, step_n, value)
 
             local time_param_n = value.time_param_n or 0
             local next_step_n = value.next_step_n or 0
@@ -233,6 +174,44 @@ proc_seats = function( mode, state_n, step_n, seats, n )
                 mode[ state_n ][ step_n ]:AddDev( n, v, group_n )
             end
             group_n = group_n + 1
+        end
+    end
+end
+
+proc_wash_data = function( mode, state_n, step_n, value)
+    --Группа устройств, управляемых по ОС с выдачей сигнала
+    if value.wash_data ~= nil then
+        --DI
+        if value.wash_data.DI ~= nil then
+            mode[ state_n ][ step_n ]:AddDev( "wash_data", 
+            value.wash_data.DI[ 1 ], 0 )
+        end
+
+        --Control signal DO
+        if value.wash_data.DO ~= nil then
+            for field, value in pairs( value.wash_data.DO ) do
+                mode[ state_n ][ step_n ]:AddDev( "wash_data", value, 1 )
+            end
+        end
+
+        --On devices.
+        if value.wash_data.devices ~= nil then
+            for field, value in pairs( value.wash_data.devices ) do
+                mode[ state_n ][ step_n ]:AddDev( "wash_data", value, 2 )
+            end
+        end
+
+        --On reverse devices.
+        if value.wash_data.rev_devices ~= nil then
+            for field, value in pairs( value.wash_data.rev_devices ) do
+                mode[ state_n ][ step_n ]:AddDev( "wash_data", value, 3 )
+            end
+        end
+
+        --Frequency param.
+        if value.wash_data.pump_freq ~= nil then
+           mode[ state_n ][ step_n ]:AddParam( "wash_data", 1,
+               value.wash_data.pump_freq )
         end
     end
 end
