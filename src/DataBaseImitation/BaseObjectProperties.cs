5c8db42085d553fe0ae936291ea25d5d57d6d405
Dzmitri Umetsky <sedrpdasavushkinby@gmail.com>
2020-04-08 14:05:36 +0300

    Added wash request parameter (#283)

--- src/DataBaseImitation/BaseObjectProperties.cs
+++ src/DataBaseImitation/BaseObjectProperties.cs
@@ -31,7 +31,9 @@ namespace DataBase
                 new NonShowedBaseProperty("DRAINAGE", "Номер шага дренаж", 
                 false),
                 new ShowedBaseProperty("OPERATION_AFTER_FILL", 
-                "Операция после наполнения")
+                "Операция после наполнения"),
+                new ShowedBaseProperty("CIP_WASH_REQUEST",
+                "Автоматическое включение мойки"),
             };
         }
 
@@ -43,7 +45,9 @@ namespace DataBase
         {
             return new List<BaseProperty>
             {
-                new ShowedBaseProperty("CIP_WASH_END", "Мойка завершена")
+                new ShowedBaseProperty("CIP_WASH_END", "Мойка завершена"),
+                new ShowedBaseProperty("CIP_WASH_REQUEST",
+                "Автоматическое включение мойки"),
             };
         }
 
@@ -70,7 +74,9 @@ namespace DataBase
             {
                 new NonShowedBaseProperty("DRAINAGE", "Номер шага дренаж",
                 false),
-                new ShowedBaseProperty("CIP_WASH_END", "Мойка завершена")
+                new ShowedBaseProperty("CIP_WASH_END", "Мойка завершена"),
+                new ShowedBaseProperty("CIP_WASH_REQUEST",
+                "Автоматическое включение мойки")
             };
         }
     }
