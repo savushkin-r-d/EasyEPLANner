0d1521452fb38c471eab8fd32c2c22b9d6c60573
dima_beel <sedrpdasavushkinby@gmail.com>
2020-05-19 16:17:39 +0300

    [fix] Work on lua initializer

--- src/Lua/sys_base_objects_initializer.lua
+++ src/Lua/sys_base_objects_initializer.lua
@@ -4,7 +4,16 @@ init_base_objects = function()
     end
 
     local objects = base_tech_objects
-    
+    for field, value in ipairs(objects) do
+        local name = value.name or ""
+        local eplanName = value.eplanName or ""
+        local s88Level = value.s88Level or 0
+        local baseOperations = value.baseOperation or { }
+        local basicName = value.basicName or ""
+        local equipment = value.equipment or { }
+        local aggregateParameters = value.aggregateParameters or { }
+
+    end
     --TODO read base tech objects and initialize in C#
 
     return 0
