c5060490f5a041743ea7fceba9122c5627a7d2e4
dima_beel <sedrpdasavushkinby@gmail.com>
2020-05-19 15:40:46 +0300

    [fix] Work on base tech objects initializer

---
+++ src/Lua/sys_base_objects_initializer.lua
@@ -0,0 +1,11 @@
+init_base_objects = function()
+    if base_tech_objects == nil then
+        return
+    end
+
+    local objects = base_tech_objects
+    
+    --TODO read base tech objects and initialize in C#
+
+    return 0
+end
