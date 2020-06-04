8019838a0b8c344fc1c713977fa07a8e465044e0
dima_beel <sedrpdasavushkinby@gmail.com>
2020-06-04 11:26:50 +0300

    [fix] Corrected equipment saving

--- src/TechObject/Equipment.cs
+++ src/TechObject/Equipment.cs
@@ -104,8 +104,14 @@ namespace TechObject
             foreach (Editor.ITreeViewItem item in items)
             {
                 var property = item as BaseParameter;
-                equipmentForSave += prefix + $"\t{property.LuaName} = " +
-                    $"\'{property.Value}\',\n";
+                bool isEmpty = property.Value == "" || 
+                    property.Value == property.DefaultValue;
+                if (!isEmpty)
+                {
+                    equipmentForSave += prefix + $"\t{property.LuaName} = " +
+                        $"\'{property.Value}\',\n";
+                }
+
             }
 
             bool needSaveQuipment = equipmentForSave != "";
