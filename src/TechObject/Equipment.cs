0afead57d5e18cf209951aad36ad4d287797d1e4
dima_beel <sedrpdasavushkinby@gmail.com>
2020-07-27 09:48:14 +0300

    [fix] Not used namespaces were deleted

--- src/TechObject/Equipment.cs
+++ src/TechObject/Equipment.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TechObject
 {
@@ -344,7 +341,6 @@ namespace TechObject
                 }
             }
         }
-
         #endregion
 
         public override string GetLinkToHelpPage()
