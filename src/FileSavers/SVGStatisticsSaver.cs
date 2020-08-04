a1853b4cea197af1835602c3820c32524293eb5a
dima_beel <sedrpdasavushkinby@gmail.com>
2020-08-04 10:56:09 +0300

    [fix] Devices count in SVG statistics file

--- src/FileSavers/SVGStatisticsSaver.cs
+++ src/FileSavers/SVGStatisticsSaver.cs
@@ -23,6 +23,7 @@ namespace EasyEPlanner
             SaveTagsCount(pathToFiles);
             SaveUnintsCount(pathToFiles);
             SaveEquipmentModulesCount(pathToFiles);
+            SaveDevicesCount(pathToFiles);
         }
 
         /// <summary>
@@ -41,7 +42,7 @@ namespace EasyEPlanner
             string[] readedFile = File.ReadAllLines(locFilePath, 
                 Encoding.GetEncoding(1251));
             int loc = readedFile.Length;
-            string strForWriting = $"{loc.ToString()} строк кода";
+            string strForWriting = $"{loc} строк кода";
             int currentValue = ValueAsPercentage(loc, maxLOCCount);
             string result = string.Format(svgFilePattern, percents, 
                 currentValue, strForWriting);
@@ -62,7 +63,7 @@ namespace EasyEPlanner
 
             folderPath += countOfTagsFileName;
             int tagsCount = XMLReporter.GetTagsCount();
-            string strForWriting = $"{tagsCount.ToString()} тэг(ов)";
+            string strForWriting = $"{tagsCount} тэг(ов)";
             int currentValue = ValueAsPercentage(tagsCount, maxTagsCount);
             string result = string.Format(svgFilePattern, percents, 
                 currentValue, strForWriting);
@@ -83,7 +84,7 @@ namespace EasyEPlanner
 
             folderPath += countOfUnitsFileName;
             int unitsCount = techObjectManager.UnitsCount;
-            string strForWriting = $"{unitsCount.ToString()} аппарат(ов)";
+            string strForWriting = $"{unitsCount} аппарат(ов)";
             int currentValue = ValueAsPercentage(unitsCount, maxUnitsCount);
             string result = string.Format(svgFilePattern, percents, 
                 currentValue, strForWriting);
@@ -103,14 +104,36 @@ namespace EasyEPlanner
         {
             const int maxEquipCount = 50;
 
-            folderPath += counstOfEquipmentModulesFileName;
+            folderPath += countOfEquipmentModulesFileName;
             int equipCount = techObjectManager.EquipmentModulesCount;
-            string strForWriting = $"{equipCount.ToString()} агрегат(ов)";
+            string strForWriting = $"{equipCount} агрегат(ов)";
             int currentValue = ValueAsPercentage(equipCount, maxEquipCount);
             string result = string.Format(svgFilePattern, percents, 
                 currentValue, strForWriting);
 
             var equipmentWriter = new StreamWriter(folderPath, false, 
+                Encoding.UTF8);
+            equipmentWriter.WriteLine(result);
+            equipmentWriter.Flush();
+            equipmentWriter.Close();
+        }
+
+        /// <summary>
+        /// Сохранить количество устройств в SVG.
+        /// </summary>
+        /// <param name="folderPath"></param>
+        private static void SaveDevicesCount(string folderPath)
+        {
+            const int maxDevicesCount = 500;
+
+            folderPath += countOfDevicesFileName;
+            int devicesCount = devicemanager.Devices.Count;
+            string strForWriting = $"{devicesCount} устройств";
+            int currentValue = ValueAsPercentage(devicesCount, maxDevicesCount);
+            string result = string.Format(svgFilePattern, percents,
+                currentValue, strForWriting);
+
+            var equipmentWriter = new StreamWriter(folderPath, false,
                 Encoding.UTF8);
             equipmentWriter.WriteLine(result);
             equipmentWriter.Flush();
@@ -125,7 +148,7 @@ namespace EasyEPlanner
         /// <returns></returns>
         private static int ValueAsPercentage(int currentValue, int maxValue)
         {
-            int result = 0;
+            int result;
             const int maxPercent = 100;
             result = (currentValue * maxPercent) / maxValue;
             return result;
@@ -136,11 +159,17 @@ namespace EasyEPlanner
         static string linesOfCodeMainProgramFileName = "lines_total.svg";
         static string countOfTagsFileName = "tags_total.svg";
         static string countOfUnitsFileName = "units_total.svg";
-        static string counstOfEquipmentModulesFileName = "agregates_total.svg";
+        static string countOfEquipmentModulesFileName = "agregates_total.svg";
+        static string countOfDevicesFileName = "devices_total.svg";
 
-        const int percents = 100; // 100% длина линии SVG. 
+        /// <summary>
+        /// 100% длина линии SVG. 
+        /// </summary>
+        const int percents = 100;
 
         static TechObjectManager techObjectManager = TechObjectManager
+            .GetInstance();
+        static Device.DeviceManager devicemanager = Device.DeviceManager
             .GetInstance();
     }
 }
