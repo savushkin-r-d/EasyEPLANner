a172a0940cd35516f0a5fd93f1f9c3a291a63e3f
dima_beel <sedrpdasavushkinby@gmail.com>
2020-11-04 09:27:04 +0300

    [fix] Use strategy to check fields in whole objects

--- src/TechObject/TechObjectChecker.cs
+++ src/TechObject/TechObjectChecker.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TechObject.CheckStrategy;
 
 namespace TechObject
 {
@@ -25,9 +23,9 @@ namespace TechObject
         {
             var errors = string.Empty;
 
-            errors += CheckTypeField();
-            errors += CheckObjectMonitorField();
-            errors += CheckEplanNameField();
+            errors += ObjectsFieldEquality(new TypeFieldEqualStrategy());
+            errors += ObjectsFieldEquality(new MonitorFieldEqualStrategy());
+            errors += ObjectsFieldEquality(new EplanNameFieldEqualStrategy());
 
             foreach (var obj in techObjectManager.TechObjects)
             {
@@ -39,76 +37,21 @@ namespace TechObject
         }
 
         /// <summary>
-        /// Проверить поле тип у объекта.
+        /// Проверить поля объектов на совпадение согласно стратегии
         /// </summary>
-        private string CheckTypeField()
-        {
-            var errorsList = new List<string>();
-            foreach (var obj in techObjectManager.TechObjects)
-            {
-                var matches = techObjectManager.TechObjects
-                    .Where(x => x.TechType == obj.TechType &&
-                    x.TechNumber == obj.TechNumber)
-                    .Select(x => techObjectManager.GetTechObjectN(x))
-                    .ToArray();
-
-                if (matches.Count() > 1)
-                {
-                    errorsList.Add($"У объектов {string.Join(",", matches)} " +
-                        $"совпадает поле \"Тип\"\n");
-                }
-            }
-
-            errorsList = errorsList.Distinct().ToList();
-            return string.Join("", errorsList);
-        }
-
-        /// <summary>
-        /// Проверить поле имени объекта Monitor у объекта.
-        /// </summary>
-        private string CheckObjectMonitorField()
-        {
-            var errorsList = new List<string>();
-            foreach (var obj in techObjectManager.TechObjects)
-            {
-                var matches = techObjectManager.TechObjects
-                    .Where(x => x.NameBC == obj.NameBC &&
-                    x.TechNumber == obj.TechNumber)
-                    .Select(x => techObjectManager.GetTechObjectN(x))
-                    .ToArray();
-
-                if (matches.Count() > 1)
-                {
-                    errorsList.Add($"У объектов {string.Join(",", matches)} " +
-                        $"совпадает поле \"Имя объекта Monitor\"\n");
-                }
-            }
-
-            errorsList = errorsList.Distinct().ToList();
-            return string.Join("", errorsList);
-        }
-
-        /// <summary>
-        /// Проверка на совпадение у ОУ у объектов с 
-        /// одинаковым базовым объектом.
-        /// </summary>
+        /// <param name="strategy">Стратегия проверки</param>
         /// <returns></returns>
-        private string CheckEplanNameField()
+        private string ObjectsFieldEquality(IFieldEqualityStrategy strategy)
         {
             var errorsList = new List<string>();
             foreach (var obj in techObjectManager.TechObjects)
             {
-                var matches = techObjectManager.TechObjects
-                    .Where(x => x.NameEplan == obj.NameEplan &&
-                    x.TechNumber == obj.TechNumber &&
-                    x.BaseTechObject?.EplanName == obj.BaseTechObject?.EplanName)
-                    .Select(x => techObjectManager.GetTechObjectN(x))
-                    .ToArray();
+                int[] matches = strategy.FindEqual(obj, techObjectManager);
 
                 if (matches.Count() > 1)
                 {
                     errorsList.Add($"У объектов {string.Join(",", matches)} " +
-                        $"совпадает поле \"ОУ\"\n");
+                        $"совпадает поле \"{strategy.FieldName}\"\n");
                 }
             }
 
