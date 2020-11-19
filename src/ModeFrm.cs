dd84e4a707b9a9498ac5380ac8097d15704eb7f8
dima_beel <sedrpdasavushkinby@gmail.com>
2020-11-19 10:10:20 +0300

    [fix] Block non safety select in attached objects

--- src/ModeFrm.cs
+++ src/ModeFrm.cs
@@ -909,6 +909,10 @@ namespace EasyEPlanner
                     }
                 }
             }
+            else
+            {
+                lastNode.CheckState = CheckState.Unchecked;
+            }
         }
 
         /// <summary>
