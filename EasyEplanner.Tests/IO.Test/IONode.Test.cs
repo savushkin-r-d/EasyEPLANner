c4aefa764995fd4321efed72703959179f1bc51d
dima_beel <sedrpdasavushkinby@gmail.com>
2020-12-28 12:25:49 +0300

    [fix] Unit-test for IONode.cs

---
+++ EasyEplanner.Tests/IO.Test/IONode.Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IO;
+using NUnit.Framework;
+
+namespace Tests.IO
+{
+    class IONodeTest
+    {
+        [TestCaseSource(nameof(TestSetTypeSource))]
+        public void TestSetType(string typeStr, IONode.TYPES expectedType)
+        {
+            // Arrange
+            string strStub = string.Empty;
+            const int intStub = 0;
+
+            // Act
+            var testNode = new IONode(typeStr, intStub, strStub, strStub);
+
+            // Assert
+            Assert.AreEqual(expectedType, testNode.Type);
+        }
+
+        private static object[] TestSetTypeSource()
+        {
+            var testData = new List<object[]>();
+
+            IONode.TYPES internal750_86x = IONode.TYPES.T_INTERNAL_750_86x;         
+            testData.Add(new object[] { "750-863", internal750_86x });
+
+            IONode.TYPES ethernet = IONode.TYPES.T_ETHERNET;
+            testData.Add(new object[] { "750-341", ethernet });
+            testData.Add(new object[] { "750-841", ethernet });
+            testData.Add(new object[] { "750-352", ethernet });
+
+            IONode.TYPES internal750_820x = IONode.TYPES.T_INTERNAL_750_820x;
+            testData.Add(new object[] { "750-8202", internal750_820x });
+            testData.Add(new object[] { "750-8203", internal750_820x });
+            testData.Add(new object[] { "750-8204", internal750_820x });
+            testData.Add(new object[] { "750-8206", internal750_820x });
+
+            IONode.TYPES pxcCoupler = IONode.TYPES.T_PHOENIX_CONTACT;
+            testData.Add(new object[] { "AXL F BK ETH", pxcCoupler });
+
+            IONode.TYPES pxcController = IONode.TYPES.T_PHOENIX_CONTACT_MAIN;
+            testData.Add(new object[] { "AXC F 2152", pxcController });
+
+            IONode.TYPES emptyType = IONode.TYPES.T_EMPTY;
+            testData.Add(new object[] { "", emptyType });
+            testData.Add(new object[] { default, emptyType });
+            testData.Add(new object[] { "Wrong type", emptyType });
+
+            return testData.ToArray();
+        }
+    }
+}
