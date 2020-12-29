675c6a4df711b0d2ddead9d1cf9a0f95ef5f9a3b
dima_beel <sedrpdasavushkinby@gmail.com>
2020-12-29 16:13:48 +0300

    [fix] Basic methods for test properties

--- EasyEplanner.Tests/IO.Test/IOModuleInfo.Test.cs
+++ EasyEplanner.Tests/IO.Test/IOModuleInfo.Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 using IO;
 using NUnit.Framework;
@@ -8,6 +9,132 @@ namespace Tests.IO
 {
     public class IOModuleInfoTest
     {
-        // TODO: Write tests
+        // TODO: Add module info test
+
+        // TODO: Get module info test
+
+        public void TestClone(IOModuleInfo testModule)
+        {
+            var cloned = (IOModuleInfo)testModule.Clone();
+
+            bool isEqual =
+                testModule.AddressSpaceType == cloned.AddressSpaceType &&
+                testModule.AICount == cloned.AICount &&
+                testModule.AOCount == cloned.AOCount &&
+                testModule.DICount == cloned.DICount &&
+                testModule.DOCount == cloned.DOCount &&
+                testModule.ChannelAddressesIn == cloned.ChannelAddressesIn &&
+                testModule.ChannelAddressesOut == cloned.ChannelAddressesOut &&
+                testModule.ChannelClamps == cloned.ChannelClamps &&
+                testModule.Description == cloned.Description &&
+                testModule.GroupName == cloned.GroupName &&
+                testModule.ModuleColor == testModule.ModuleColor &&
+                testModule.Name == cloned.Name &&
+                testModule.Number == cloned.Number &&
+                testModule.TypeName == cloned.TypeName;
+
+            Assert.IsTrue(isEqual);
+        }
+
+        public void TestSetGetName(IOModuleInfo testModule, string expected,
+            string actual)
+        {
+            testModule.Name = actual;
+            Assert.AreEqual(expected, testModule.Name);
+        }
+
+        public void TestSetGetNumber(IOModuleInfo testModule, int expected,
+            int actual)
+        {
+            testModule.Number = actual;
+            Assert.AreEqual(expected, testModule.Number);
+        }
+
+        public void TestSetGetDescription(IOModuleInfo testModule,
+            string expected, string actual)
+        {
+            testModule.Description = actual;
+            Assert.AreEqual(expected, testModule.Description);
+        }
+
+        public void TestSetGetAddressSpaceType(IOModuleInfo testModule,
+            IOModuleInfo.ADDRESS_SPACE_TYPE expected,
+            IOModuleInfo.ADDRESS_SPACE_TYPE actual)
+        {
+            testModule.AddressSpaceType = actual;
+            Assert.AreEqual(expected, testModule.AddressSpaceType);
+        }
+
+        public void TestSetGetChannelClamps(IOModuleInfo testModule,
+            int[] expected, int[] actual)
+        {
+            testModule.ChannelClamps = actual;
+            Assert.AreEqual(expected, testModule.ChannelClamps);
+        }
+
+        public void TestSetGetChannelAddressesOut(IOModuleInfo testModule,
+            int[] expected, int[] actual)
+        {
+            testModule.ChannelAddressesOut = actual;
+            Assert.AreEqual(expected, testModule.ChannelAddressesOut);
+        }
+
+        public void TestSetGetChannelAddressesIn(IOModuleInfo testModule,
+            int[] expected, int[] actual)
+        {
+            testModule.ChannelAddressesIn = actual;
+            Assert.AreEqual(expected, testModule.ChannelAddressesIn);
+        }
+
+        public void TestSetGetAICount(IOModuleInfo testModule,
+            int expected, int actual)
+        {
+            testModule.AICount = actual;
+            Assert.AreEqual(expected, testModule.DOCount);
+        }
+
+        public void TestSetGetAOCount(IOModuleInfo testModule,
+            int expected, int actual)
+        {
+            testModule.AOCount = actual;
+            Assert.AreEqual(expected, testModule.DOCount);
+        }
+
+        public void TestSetGetDICount(IOModuleInfo testModule,
+            int expected, int actual)
+        {
+            testModule.DICount = actual;
+            Assert.AreEqual(expected, testModule.DOCount);
+        }
+
+        public void TestSetGetDOCount(IOModuleInfo testModule,
+            int expected, int actual)
+        {
+            testModule.DOCount = actual;
+            Assert.AreEqual(expected, testModule.DOCount);
+        }
+
+        public void TestSetGetTypeName(IOModuleInfo testModule,
+            string expected, string actual)
+        {
+            testModule.TypeName = actual;
+            Assert.AreEqual(expected, testModule.TypeName);
+        }
+
+        public void TestSetGetModuleColor(IOModuleInfo testModule,
+            Color expected, Color actual)
+        {
+            testModule.ModuleColor = actual;
+            Assert.AreEqual(expected, testModule.ModuleColor);
+        }
+
+        public void TestSetGetGroupName(IOModuleInfo testModule,
+            string expected, string actual)
+        {
+            testModule.GroupName = actual;
+            Assert.AreEqual(expected, testModule.GroupName);
+        }
+
+        // TODO: Write getter for simple IOModule for test getters and setters
     }
 }
