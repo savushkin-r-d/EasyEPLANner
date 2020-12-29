a0e1f72ef5b5a161f6bb2212cdba9b652c9a316f
dima_beel <sedrpdasavushkinby@gmail.com>
2020-12-29 12:28:30 +0300

    [fix] Initial test class

---
+++ EasyEplanner.Tests/IO.Test/IOModuleInfo.Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IO;
+using NUnit.Framework;
+
+namespace Tests.IO
+{
+    public class IOModuleInfoTest
+    {
+        // TODO: Write tests
+    }
+}
