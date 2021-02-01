6354c2acd0b65dd92d9fde26f7df9380d86fd94d
dima_beel <sedrpdasavushkinby@gmail.com>
2021-02-01 11:56:39 +0300

    [fix] BaseTechobject test for new feature

---
+++ EasyEplanner.Tests/TechObject.Test/Base.Test/BaseTechObject.Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechObject;
+using NUnit.Framework;
+
+namespace Tests.TechObject
+{
+    class BaseTechObjectTest
+    {
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(100)]
+        public void AddSystemParameter_EmptyBaseTechObject_AddParametersToList(
+            int parametersCount)
+        {
+            BaseTechObject emptyObject = GetEmpty();
+            for (int i = 0; i < parametersCount; i++)
+            {
+                emptyObject.AddSystemParameter("LuaName", "Name", 0, "шт");
+            }
+
+            Assert.AreEqual(parametersCount, emptyObject.SystemParams.Count);
+        }
+
+        private BaseTechObject GetEmpty()
+        {
+            return new BaseTechObject();
+        }
+    }
+}
