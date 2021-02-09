76f5e6faf46fea4f593f5d024d1448a4cf62068d
dima_beel <sedrpdasavushkinby@gmail.com>
2021-02-09 12:03:15 +0300

    [fix] Unit-tests

---
+++ EasyEplanner.Tests/TechObject.Test/Base.Test/BaseParameter.Test.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using TechObject;
+
+namespace EasyEplanner.Tests
+{
+    public class BaseParameterTest
+    {
+        /// <summary>
+        /// Реализация абстрактного класса BaseParameter для его тестирования
+        /// </summary>
+        class BaseParameterImplementation : BaseParameter
+        {
+            public BaseParameterImplementation(string luaName, string name,
+                string defaultValue = "",
+                List<DisplayObject> displayObjects = null)
+                : base(luaName, name, default, displayObjects) { }
+
+            public override BaseParameter Clone()
+            {
+                return new BaseParameterImplementation(LuaName, Name,
+                    DefaultValue, DisplayObjects);
+            }
+        }
+
+        [Test]
+        public void DisplayObjectsFromNewObj_EmptyObject_ReturnsNoDisplayObjects()
+        {
+            var nonDisplayObjects = new List<BaseParameter.DisplayObject>
+            {
+                BaseParameter.DisplayObject.None
+            };
+
+            var newObj = new BaseParameterImplementation(stub, stub);
+
+            Assert.AreEqual(nonDisplayObjects, newObj.DisplayObjects);
+        }
+
+        [Test]
+        public void DisplayObjectsFromNewObj_EmptyObject_ReturnsAllDisplayObjects()
+        {
+            var allDisplayObjects = new List<BaseParameter.DisplayObject>
+            {
+                BaseParameter.DisplayObject.Signals,
+                BaseParameter.DisplayObject.Parameters,
+            };
+
+            var newObj = new BaseParameterImplementation(stub, stub, stub,
+                allDisplayObjects);
+
+            Assert.AreEqual(allDisplayObjects, newObj.DisplayObjects);
+        }
+
+        [Test]
+        public void CreateObject_NoObject_ReturnsObject()
+        {
+            var obj = new BaseParameterImplementation(stub, stub);
+            string[] expectedEditText = new string[] { obj.LuaName, obj.Value };
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(obj.LuaName, stub);
+                Assert.AreEqual(obj.Name, stub);
+                Assert.IsTrue(obj.IsUseDevList);
+                Assert.IsFalse(obj.IsEmpty);
+                Assert.IsNull(obj.Owner);
+                Assert.AreEqual(expectedEditText, obj.EditText);
+                Assert.AreEqual(obj.CurrentValueType,
+                    BaseParameter.ValueType.None);
+            });
+        }
+
+        string stub = string.Empty;
+    }
+}
