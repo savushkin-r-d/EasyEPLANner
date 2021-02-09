7f2239385ba1981db97364b87156b1cc2031ff87
dima_beel <sedrpdasavushkinby@gmail.com>
2021-02-09 14:51:03 +0300

    [fix] GetDisplayObjects and AddDisplayObject test

--- EasyEplanner.Tests/TechObject.Test/Base.Test/BaseParameter.Test.cs
+++ EasyEplanner.Tests/TechObject.Test/Base.Test/BaseParameter.Test.cs
@@ -96,6 +96,153 @@ namespace EasyEplanner.Tests
             });
         }
 
+        [TestCaseSource(nameof(AddDisplayObjectCaseSource))]
+        public void AddDisplayObject_EmptyParameter_AddArgumentOrReplac(
+            List<string> actualEnumNames,
+            List<BaseParameter.DisplayObject> expected)
+        {
+            var newObject = new BaseParameterImplementation(stub, stub);
+
+            foreach(var displayObject in actualEnumNames)
+            {
+                newObject.AddDisplayObject(displayObject);
+            }
+
+            Assert.AreEqual(expected, newObject.DisplayObjects);
+        }
+
+        private static object[] AddDisplayObjectCaseSource()
+        {
+            return new object[]
+            {
+                new object[]
+                {
+                    new List<string>
+                    {
+                        "none"
+                    },
+                    new List<BaseParameter.DisplayObject>
+                    {
+                        BaseParameter.DisplayObject.None,
+                    },
+                },
+                new object[]
+                {
+                    new List<string>
+                    {
+                        "signals"
+                    },
+                    new List<BaseParameter.DisplayObject>
+                    {
+                        BaseParameter.DisplayObject.Signals,
+                    },
+                },
+                new object[]
+                {
+                    new List<string>
+                    {
+                        "parameters"
+                    },
+                    new List<BaseParameter.DisplayObject>
+                    {
+                        BaseParameter.DisplayObject.Parameters,
+                    },
+                },
+                new object[]
+                {
+                    new List<string>
+                    {
+                        "parameters",
+                        "signals"
+                    },
+                    new List<BaseParameter.DisplayObject>
+                    {
+                        BaseParameter.DisplayObject.Parameters,
+                        BaseParameter.DisplayObject.Signals,
+                    },
+                },
+            };
+        }
+
+        [TestCaseSource(nameof(GetDisplayObjectsCaseSource))]
+        public void GetDisplayObjects_NewObject_ReturnsExpectedValues(
+            List<BaseParameter.DisplayObject> displayObjects,
+            Device.DeviceType[] expectedDevTypes,
+            Device.DeviceSubType[] expectedDevSubTypes,
+            bool expectedDisplayParameters)
+        {
+            var newObj = new BaseParameterImplementation(stub, stub, stub,
+                displayObjects);
+
+            newObj.GetDisplayObjects(out Device.DeviceType[] devTypes,
+                out Device.DeviceSubType[] devSubTypes,
+                out bool displayParameters);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedDevTypes, devTypes);
+                Assert.AreEqual(expectedDevSubTypes, devSubTypes);
+                Assert.AreEqual(expectedDisplayParameters, displayParameters);
+            });
+        }
+        
+        private static object[] GetDisplayObjectsCaseSource()
+        {
+            return new object[]
+            {
+                new object[]
+                {
+                    new List<BaseParameter.DisplayObject>
+                    {
+                        BaseParameter.DisplayObject.Signals,
+                        BaseParameter.DisplayObject.Parameters
+                    },
+                    new Device.DeviceType[]
+                    {
+                        Device.DeviceType.AI,
+                        Device.DeviceType.AO,
+                        Device.DeviceType.DI,
+                        Device.DeviceType.DO
+                    },
+                    null,
+                    true,
+                },
+                new object[]
+                {
+                    new List<BaseParameter.DisplayObject>
+                    {
+                        BaseParameter.DisplayObject.Signals,
+                    },
+                    new Device.DeviceType[]
+                    {
+                        Device.DeviceType.AI,
+                        Device.DeviceType.AO,
+                        Device.DeviceType.DI,
+                        Device.DeviceType.DO
+                    },
+                    null,
+                    false,
+                },
+                new object[]
+                {
+                    new List<BaseParameter.DisplayObject>
+                    {
+                        BaseParameter.DisplayObject.Parameters
+                    },
+                    new Device.DeviceType[0],
+                    null,
+                    true,
+                },
+                new object[]
+                {
+                    null,
+                    new Device.DeviceType[0],
+                    null,
+                    false,
+                },
+            };
+        }
+
         string stub = string.Empty;
     }
 }
