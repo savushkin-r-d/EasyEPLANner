10e22dfc1c2ac043bbafa8e7025d9235984b721b
dima_beel <dima.beel.dima@gmail.com>
2021-11-12 10:25:34 +0300

    Update version of Add-In to November (2021.11)

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.10.*")]
-[assembly: AssemblyFileVersion("2021.10.0.0")]
+[assembly: AssemblyVersion("2021.11.*")]
+[assembly: AssemblyFileVersion("2021.11.0.0")]
