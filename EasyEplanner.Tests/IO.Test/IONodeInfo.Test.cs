2f10ee7a6737ae63918d8ffd2dd9085968476bf7
dima_beel <dima.beel.dima@gmail.com>
2022-01-05 11:27:01 +0300

    Update unit-tests

--- EasyEplanner.Tests/IO.Test/IONodeInfo.Test.cs
+++ EasyEplanner.Tests/IO.Test/IONodeInfo.Test.cs
@@ -15,30 +15,67 @@ namespace Tests.IO
         public void AddNodeInfo_Adds_New_NodeInfo()
         {
             int expectedCount = 4;
-            IONodeInfo.AddNodeInfo("750-362", IONode.TYPES.T_ETHERNET, true);
-            IONodeInfo.AddNodeInfo("AXC F BK ETH", IONode.TYPES.T_PHOENIX_CONTACT, false);
-            IONodeInfo.AddNodeInfo("750-352", IONode.TYPES.T_ETHERNET, true);
-            IONodeInfo.AddNodeInfo("AXC F 1552", IONode.TYPES.T_PHOENIX_CONTACT_MAIN, false);
+            var nodes = new[]
+            {
+                new { Name = "750-362", Type = IONode.TYPES.T_ETHERNET, IsCoupler = true },
+                new { Name = "AXC F BK ETH", Type = IONode.TYPES.T_PHOENIX_CONTACT, IsCoupler = false },
+                new { Name = "750-352", Type = IONode.TYPES.T_ETHERNET, IsCoupler = true },
+                new { Name = "AXC F 1552", Type = IONode.TYPES.T_PHOENIX_CONTACT_MAIN, IsCoupler = false },
+            };
+
+            foreach(var node in nodes)
+            {
+                IONodeInfo.AddNodeInfo(node.Name, node.Type, node.IsCoupler);
+            }
 
             int actualCount = IONodeInfo.Count;
-
-            Assert.AreEqual(expectedCount, actualCount);
-            Assert.AreEqual(expectedCount, IONodeInfo.Nodes.Count);
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedCount, actualCount);
+                Assert.AreEqual(expectedCount, IONodeInfo.Nodes.Count);
+                for(int i = 0; i < IONodeInfo.Nodes.Count; i++)
+                {
+                    var addedNodes = IONodeInfo.Nodes;
+                    Assert.AreEqual(nodes[i].Name, addedNodes[i].Name);
+                    Assert.AreEqual(nodes[i].Type, addedNodes[i].Type);
+                    Assert.AreEqual(nodes[i].IsCoupler, addedNodes[i].IsCoupler);
+                }
+            });
         }
 
         [Test]
         public void AddNodeInfo_Do_Not_Add_Already_Added_Nodes()
         {
             int expectedCount = 2;
-            IONodeInfo.AddNodeInfo("750-362", IONode.TYPES.T_ETHERNET, true);
-            IONodeInfo.AddNodeInfo("AXC F BK ETH", IONode.TYPES.T_PHOENIX_CONTACT, false);
-            IONodeInfo.AddNodeInfo("750-362", IONode.TYPES.T_ETHERNET, true);
-            IONodeInfo.AddNodeInfo("AXC F BK ETH", IONode.TYPES.T_PHOENIX_CONTACT, false);
+            var nodes = new[]
+            {
+                new { Name = "750-362", Type = IONode.TYPES.T_ETHERNET, IsCoupler = true },
+                new { Name = "AXC F BK ETH", Type = IONode.TYPES.T_PHOENIX_CONTACT, IsCoupler = false },
+            };
+
+            int repeatCount = 2;
+            for(int i = 0; i < repeatCount; i++)
+            {
+                foreach (var node in nodes)
+                {
+                    IONodeInfo.AddNodeInfo(node.Name, node.Type, node.IsCoupler);
+                }
+            }
+
 
             int actualCount = IONodeInfo.Count;
-
-            Assert.AreEqual(expectedCount, actualCount);
-            Assert.AreEqual(expectedCount, IONodeInfo.Nodes.Count);
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedCount, actualCount);
+                Assert.AreEqual(expectedCount, IONodeInfo.Nodes.Count);
+                for (int i = 0; i < IONodeInfo.Nodes.Count; i++)
+                {
+                    var addedNodes = IONodeInfo.Nodes;
+                    Assert.AreEqual(nodes[i].Name, addedNodes[i].Name);
+                    Assert.AreEqual(nodes[i].Type, addedNodes[i].Type);
+                    Assert.AreEqual(nodes[i].IsCoupler, addedNodes[i].IsCoupler);
+                }
+            });
         }
 
         [Test]
