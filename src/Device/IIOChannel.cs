590770699095e84cf21bed81759ef80bd0298549
dima_beel <dima.beel.dima@gmail.com>
2022-02-15 11:39:40 +0300

    Move inner classes from IODevice to own classes files

---
+++ src/Device/IIOChannel.cs
@@ -0,0 +1,33 @@
+﻿namespace EplanDevice
+{
+    public partial class IODevice
+    {
+        public interface IIOChannel
+        {
+            /// <summary>
+            /// Логический номер клеммы (порядковый)
+            /// </summary>
+            int LogicalClamp { get; }
+
+            /// <summary>
+            /// Имя канала (DI,DO, AI,AO)
+            /// </summary>
+            string Name { get; }
+
+            /// <summary>
+            /// Комментарий
+            /// </summary>
+            string Comment { get; }
+
+            /// <summary>
+            /// Сдвиг начала модуля
+            /// </summary>
+            int ModuleOffset { get; }
+
+            /// <summary>
+            /// Полный номер модуля
+            /// </summary>
+            int FullModule { get; }
+        }
+    }
+}
---
+++ src/Device/IOLinkSize.cs
@@ -0,0 +1,48 @@
+﻿namespace EplanDevice
+{
+    public partial class IODevice
+    {
+        /// <summary>
+        /// Размер IO-Link областей устройства ввода-вывода.
+        /// </summary>
+        public class IOLinkSize
+        {
+            public IOLinkSize()
+            {
+                SizeIn = 0;
+                SizeOut = 0;
+            }
+
+            /// <summary>
+            /// Возвращает максимальны размер байтовой области для модулей ввода
+            /// вывода при расчете IO-Link адресов если используется
+            /// Phoenix Contact
+            /// </summary>
+            /// <returns></returns>
+            public int GetMaxIOLinkSize()
+            {
+                return SizeOut > SizeIn ? SizeOut : SizeIn;
+            }
+
+            /// <summary>
+            /// Размер области входа приведенный к слову (целому)
+            /// </summary>
+            public int SizeIn { get; set; }
+
+            /// <summary>
+            /// Размер области выхода приведенный к слову (целому)
+            /// </summary>
+            public int SizeOut { get; set; }
+
+            /// <summary>
+            /// Размер области входа из файла
+            /// </summary>
+            public float SizeInFromFile { get; set; }
+
+            /// <summary>
+            /// Размер области выхода из файла
+            /// </summary>
+            public float SizeOutFromFile { get; set; }
+        }
+    }
+}
