20da31a3c90e851f58678670fa8108ba08efa64b
dima_beel <dima.beel.dima@gmail.com>
2022-02-16 11:09:52 +0300

    Device, Devices, Sensor unit-tests

--- EasyEplanner.Tests/PxcIolinkConfiguration/Models/Device.Test.cs
+++ EasyEplanner.Tests/PxcIolinkConfiguration/Models/Device.Test.cs
@@ -1,10 +1,130 @@
-﻿namespace Tests.PxcIolinkConfigration.Models
+﻿using EasyEPlanner.PxcIolinkConfiguration.Models;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tests.PxcIolinkConfigration.Models
 {
     public class DeviceTest
     {
-        //Add
-        //ShouldSerializePort
-        //ShouldSerializeDevices
-        //IsEmpty
+        [Test]
+        public void ShouldSerializePort_DeviceWithZeroPort_ReturnsFalse()
+        {
+            var device = new Device();
+
+            Assert.IsFalse(device.ShouldSerializePort());
+        }
+
+        [Test]
+        public void ShouldSerializePort_DeviceNotZeroPort_ReturnsTrue()
+        {
+            var device = new Device();
+            device.Port = 1;
+
+            Assert.IsTrue(device.ShouldSerializePort());
+        }
+
+        [Test]
+        public void ShouldSerializeDevices_DeviceWithNullDevices_ReturnsFalse()
+        {
+            var device = new Device();
+
+            Assert.IsFalse(device.ShouldSerializeDevices());
+        }
+
+        [Test]
+        public void ShouldSerializeDevices_DeviceWithNotNullDevicesEmptyList_ReturnsFalse()
+        {
+            var device = new Device();
+            device.Devices = new Devices();
+
+            Assert.IsFalse(device.ShouldSerializeDevices());
+        }
+
+        [Test]
+        public void ShouldSerializeDevices_DeviceNotNullDevicesNotEmpty_ReturnsTrue()
+        {
+            var device = new Device();
+            device.Add(new List<Device>
+            { 
+                new Device()
+            });
+
+            Assert.IsTrue(device.ShouldSerializeDevices());
+        }
+
+        [Test]
+        public void IsEmpty_NewDevice_ReturnsTrue()
+        {
+            var device = new Device();
+
+            Assert.IsTrue(device.IsEmpty());
+        }
+
+        [Test]
+        public void IsEmpty_SensorIsNotNull_ReturnsFalse()
+        {
+            var device = new Device();
+            device.Sensor = new Sensor()
+            {
+                VendorId = 100,
+            };
+
+            Assert.IsFalse(device.IsEmpty());
+        }
+
+        [Test]
+        public void IsEmpty_PortNotZero_ReturnsFalse()
+        {
+            var device = new Device();
+            device.Port = 1;
+
+            Assert.IsFalse(device.IsEmpty());
+        }
+
+        [Test]
+        public void IsEmpty_ParametersNotNull_ReturnsFalse()
+        {
+            var device = new Device();
+            device.Parameters = new Parameters()
+            {
+                Param = new List<Param>()
+                {
+                    new Param()
+                }
+            };
+
+            Assert.IsFalse(device.IsEmpty());
+        }
+
+        [Test]
+        public void IsEmpty_DevicesIsNotNull_ReturnsFalse()
+        {
+            var device = new Device();
+            device.Devices = new Devices()
+            {
+                Device = new List<Device>
+                {
+                    new Device()
+                }
+            };
+
+            Assert.IsFalse(device.IsEmpty());
+        }
+
+        [Test]
+        public void Add_AddNewInstanseOfDevices_SuccessfullAdd()
+        {
+            var device = new Device();
+            var devices = new List<Device>()
+            {
+                new Device(),
+                new Device()
+            };
+
+            device.Add(devices);
+
+            Assert.IsFalse(device.IsEmpty());
+            Assert.AreEqual(devices.GetHashCode(), device.Devices.Device.GetHashCode());
+        }
     }
 }
--- EasyEplanner.Tests/PxcIolinkConfiguration/Models/Devices.Test.cs
+++ EasyEplanner.Tests/PxcIolinkConfiguration/Models/Devices.Test.cs
@@ -1,7 +1,31 @@
-﻿namespace Tests.PxcIolinkConfigration.Models
+﻿using EasyEPlanner.PxcIolinkConfiguration.Models;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tests.PxcIolinkConfigration.Models
 {
     public class DevicesTest
     {
-        //IsEmpty
+        [Test]
+        public void IsEmpty_NewDevices_ReturnsTrue()
+        {
+            var devices = new Devices();
+
+            Assert.IsTrue(devices.IsEmpty());
+        }
+
+        [Test]
+        public void IsEmpty_DeviceNotEmpty_ReturnsFalse()
+        {
+            var devices = new Devices()
+            {
+                Device = new List<Device>
+                {
+                    new Device()
+                }
+            };
+
+            Assert.IsFalse(devices.IsEmpty());
+        }
     }
 }
