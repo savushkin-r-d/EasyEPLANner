b799b8c71596d2d79ebbd74b77e249d47a607382
dima_beel <dima.beel.dima@gmail.com>
2022-02-16 13:01:26 +0300

    Add unit-tests to LinerecorderMultiSensor and Param

--- EasyEplanner.Tests/PxcIolinkConfiguration/Models/LinerecorderMultiSensor.Test.cs
+++ EasyEplanner.Tests/PxcIolinkConfiguration/Models/LinerecorderMultiSensor.Test.cs
@@ -1,8 +1,55 @@
-﻿namespace Tests.PxcIolinkConfigration.Models
+﻿using EasyEPlanner.PxcIolinkConfiguration.Models;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.PxcIolinkConfigration.Models
 {
-    public class LinerecorderMultiSensor
+    public class LinerecorderMultiSensorTest
     {
-        //Add
-        //IsEmpty
+        [Test]
+        public void Add_NewSensor_CorrectAdd()
+        {
+            var sensor = new LinerecorderMultiSensor();
+            Device device = new Device()
+            {
+                Sensor = new Sensor(),
+                Devices = new Devices(),
+                Parameters = new Parameters(),
+                Port = 1,
+            };
+
+            sensor.Add(device);
+
+            int expectedDevicesCount = 1;
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedDevicesCount, sensor.Devices.Device.Count);
+                Assert.AreEqual(device.GetHashCode(), sensor.Devices.Device.First().GetHashCode());
+            });
+        }
+
+        [Test]
+        public void IsEmpty_NewSensor_ReturnsTrue()
+        {
+            var sensor = new LinerecorderMultiSensor();
+
+            Assert.IsTrue(sensor.IsEmpty());
+        }
+
+        [Test]
+        public void IsEmpty_NewSensorNotEmptyDevices_ReturnsFalse()
+        {
+            var sensor = new LinerecorderMultiSensor();
+            sensor.Devices = new Devices()
+            {
+                Device = new List<Device>
+                {
+                    new Device(){ }
+                }
+            };
+
+            Assert.IsFalse(sensor.IsEmpty());
+        }
     }
 }
--- EasyEplanner.Tests/PxcIolinkConfiguration/Models/Param.Test.cs
+++ EasyEplanner.Tests/PxcIolinkConfiguration/Models/Param.Test.cs
@@ -1,7 +1,36 @@
-﻿namespace Tests.PxcIolinkConfigration.Models
+﻿using EasyEPlanner.PxcIolinkConfiguration.Models;
+using NUnit.Framework;
+
+namespace Tests.PxcIolinkConfigration.Models
 {
     public class ParamTest
     {
-        //Clone
+        [Test]
+        public void Clone_FilledParam_EqualValues()
+        {
+            var paramToClone = new Param()
+            {
+                Text = "1",
+                Unit = "2",
+                Value = "3",
+                InternalValue = "4",
+                Id = "5",
+                Name = "6",
+                Subindex = "7"
+            };
+
+            var cloned = (Param)paramToClone.Clone();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(paramToClone.Text, cloned.Text);
+                Assert.AreEqual(paramToClone.Unit, cloned.Unit);
+                Assert.AreEqual(paramToClone.Value, cloned.Value);
+                Assert.AreEqual(paramToClone.InternalValue, cloned.InternalValue);
+                Assert.AreEqual(paramToClone.Id, cloned.Id);
+                Assert.AreEqual(paramToClone.Name, cloned.Name);
+                Assert.AreEqual(paramToClone.Subindex, cloned.Subindex);
+            });
+        }
     }
 }
