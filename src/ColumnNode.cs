3bac89f046b5d0ec25ae8e4837a47416b962025c
GutyrchikKirill <gutyrcikkirill@gmail.com>
2022-06-13 15:08:52 +0300

    Edit ColumnNode

--- src/ColumnNode.cs
+++ src/ColumnNode.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Aga.Controls.Tree;
+﻿using Aga.Controls.Tree;
 
 namespace EasyEPlanner
 {
