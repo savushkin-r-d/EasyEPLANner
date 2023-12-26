f5db40822066ddf6545d10cc8af89b04f2dae862
KirillGutyrchik <gutyrcikkirill@gmail.com>
2023-12-26 16:05:46 +0300

    Add eplanner action to launch Updater (#1298)

---
+++ src/Main/OpenUpdaterAction.cs
@@ -0,0 +1,43 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace EasyEPlanner
+{
+    /// <summary>
+    /// Действие для открытия средства обновления надстройки
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class OpenUpdater : IEplAction
+    {
+        ~OpenUpdater() { }
+
+        public bool Execute(ActionCallingContext ctx)
+        {
+            var path = new FileInfo(AddInModule.OriginalAssemblyPath).Directory.FullName;
+            if (File.Exists(path + AddInModule.LauncherPath))
+            {
+                var proc = Process.Start(path + AddInModule.LauncherPath, $"{AddInModule.RunUpdaterFromMenu} {Process.GetCurrentProcess().Id}");
+                proc.WaitForExit();
+                return true;
+            }
+
+            MessageBox.Show($"Средство обновления {AddInModule.LauncherPath} не найдено!",
+                "Eplanner", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return false;
+        }
+
+        public bool OnRegister(ref string Name, ref int Ordinal)
+        {
+            Name = nameof(OpenUpdater);
+
+            return true;
+        }
+
+        public void GetActionProperties(ref ActionProperties actionProperties) { }
+    }
+}
