3fb26a1978a763ed8357ecff9643fb8b87807a04
KirillGutyrchik <gutyrcikkirill@gmail.com>
2024-04-22 16:22:43 +0300

    Fix lua reader script

--- src/Lua/sys.lua
+++ src/Lua/sys.lua
@@ -166,7 +166,7 @@ function proc_operation_devices(value, mode, state_n)
             local time_param_n = value.time_param_n or 0
             local step_max_duration_par_n = value.step_max_duration_par_n or -1
             local attached_object = value.attached_object or -1
-            if time_param_n > 0 then
+            if time_param_n > 0 or step_max_duration_par_n then
                 step:SetPar(time_param_n, next_step_n, step_max_duration_par_n)
             end
             step:SetAttachedObject(attached_object)
