682eb8e11765870f49d2f765fbeda51544b50348
KirillGutyrchik <gutyrcikkirill@gmail.com>
2024-08-23 14:25:45 +0300

    Fix comment

--- src/Main/ImportIcpWagoModulesAction.cs
+++ src/Main/ImportIcpWagoModulesAction.cs
@@ -47,9 +47,11 @@ namespace EasyEPlanner
                 }
 
                 var data = new StreamReader(openFileDialog.FileName, EncodingDetector.DetectFileEncoding(openFileDialog.FileName), true).ReadToEnd();
-                // Fix main.wago.plua '}'
+
+                // Fix main.wago.plua file: add "," to table before "group_dev_ex"
                 data = Regex.Replace(data, @"}(?=(\r|\n|\r\n)\t+group_dev_ex)", "},", RegexOptions.None, TimeSpan.FromMilliseconds(100));
-                // Remove unnecessary lua-module
+                
+                // Remove unnecessary lua-module 'sys_wago'
                 data = Regex.Replace(data, "require 'sys_wago'", "", RegexOptions.None, TimeSpan.FromMilliseconds(100));
 
                 var modulesImporter = new ModulesImporter(currentProject, data);
