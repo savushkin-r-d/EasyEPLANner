b7e9cb2d29eaed387f5f48f786db72e87aa22b6c
KirillGutyrchik <gutyrcikkirill@gmail.com>
2025-03-10 12:49:13 +0300

    fix test

--- EasyEplanner.Tests/TechObject.Test/Base.Test/Properties.Test/MainAggregateParameter.Test.cs
+++ EasyEplanner.Tests/TechObject.Test/Base.Test/Properties.Test/MainAggregateParameter.Test.cs
@@ -66,7 +66,7 @@ namespace TechObject.Tests
 
                 Assert.AreEqual("FILL_PARAMETER", param.GetNameLua());
                 Assert.AreEqual("Наполнение. Параметр", param.GetName());
-                Assert.AreEqual(1, param.Operations);
+                Assert.AreEqual("1", param.Operations);
             });
         }
     }
