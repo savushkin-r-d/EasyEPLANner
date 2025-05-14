88ae5c0e59d111b2421800737e97f9ad57ffc977
KirillGutyrchik <gutyrcikkirill@gmail.com>
2025-05-14 12:09:26 +0300

    Check PID properties binded device

--- src/Device/IODevices/C.cs
+++ src/Device/IODevices/C.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using StaticHelper;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Windows.Forms;
@@ -108,6 +109,31 @@ namespace EplanDevice
             }
 
             return null;
+        }
+
+        public override string Check()
+        {
+            string res = base.Check();
+
+            res += CheckDeviceProperty(Property.IN_VALUE);
+            res += CheckDeviceProperty(Property.OUT_VALUE);
+
+            return res;
+        }
+
+        private string CheckDeviceProperty(string property)
+        {
+            if (properties.TryGetValue(property, out var value) &&
+                value is string dev &&
+                dev != string.Empty &&
+                DeviceManager.GetInstance().GetDevice(dev).Description is CommonConst.Cap)
+            {
+                properties[property] = string.Empty;
+                UpdateProperties();
+                return $"{Name}: в свойстве {property} сброшено несуществующее устройство {dev}.\n";
+            }
+
+            return string.Empty;
         }
 
         #region сохранение в Lua
