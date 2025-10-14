379ecf6f9de283b6d8b7f4fe481e9ab1055e249d
KirillGutyrchik <gutyrcikkirill@gmail.com>
2025-10-14 14:55:31 +0300

    exclude from code coverage

--- src/TechObject/TechObjectXMLMaker.cs
+++ src/TechObject/TechObjectXMLMaker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows.Forms;
 
 namespace TechObject
 {
+    [ExcludeFromCodeCoverage]
     public class TechObjectXMLMaker
     {
         public TechObjectXMLMaker(ITechObjectManager techObjectManager)
