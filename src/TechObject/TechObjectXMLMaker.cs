1452e2c6d2b492397455d0294823d5deb19ba067
KirillGutyrchik <gutyrcikkirill@gmail.com>
2025-11-03 08:34:49 +0300

    [XML chbase] Object parameters by name

--- src/TechObject/TechObjectXMLMaker.cs
+++ src/TechObject/TechObjectXMLMaker.cs
@@ -142,6 +142,8 @@ namespace TechObject
                 {
                     root.AddChannel(
                         SubtypeName(subtype, "Параметры"),
+                        cdbxNewNames ?
+                        $"{objName}.{paramsGroup.NameForChannelBase}_{param.GetNameLua()}[ {param.GetParameterNumber} ]" :
                         $"{objName}.{paramsGroup.NameForChannelBase}[ {param.GetParameterNumber} ]");
                 }
             }
@@ -191,7 +193,7 @@ namespace TechObject
         /// <returns></returns>
         private string GenerateObjectName(TechObject item, int itemNumber)
         {
-            if (cdbxNewNames == true)
+            if (cdbxNewNames)
             {
                 return item.NameBC.ToUpper() + item.TechNumber.ToString();
             }
