59d6e730bac1ccdcdac6ec905decc7b1ad095a9a
KirillGutyrchik <gutyrcikkirill@gmail.com>
2026-01-27 08:53:09 +0300

    [mpk] Generation text components files *.mpkx (#1719)
    Fixes #1711.
    
    ```ChangeLog
    Генерация файлов компонентов *.mpkx;
    ```
    
    ---------
    
    Co-authored-by: Dzmitry Ivaniuk <dzimitriy@gmail.com>

---
+++ src/mpk/ViewModel/MpkSaverContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyEPlanner.mpk.ViewModel
+{
+    public class MpkSaverContext(string projectName) : IMpkSaverContext
+    {
+        private const string BaseMpkDirectory = "P:\\Monitor\\Mpk";
+
+        public string MpkDirectory { get; set; } = Path.Combine(BaseMpkDirectory, projectName);
+
+        public string MainContainerName { get; set; } = projectName;
+
+        public bool Rewrite { get; set; } = true;
+    }
+}
