e132c42afb806dca1c043e6952a7f5613e165766
KirillGutyrchik <gutyrcikkirill@gmail.com>
2026-05-27 15:56:32 +0300

    fix duplicates and test coverage

--- src/IO/ViewModel/DeletedModule.cs
+++ src/IO/ViewModel/DeletedModule.cs
@@ -1,42 +1,13 @@
-using System.Drawing;
-
 namespace IO.ViewModel
 {
-    public class DeletedModule : IHasIcon, IToolTip
+    public class DeletedModule : ModuleViewItem
     {
-        private readonly IIOModule module;
-
         public DeletedModule(IIOModule module)
+            : base(module)
         {
-            this.module = module;
         }
 
-        public string Name => module.Name;
-
-        public string Description => module.Info.Description;
-
-        public IIOModule IOModule => module;
-
-        string IToolTip.Name => module.Function?.VisibleName ?? Name;
-
-        string IToolTip.Description =>
-            $"Артикул: {module.Info.Name}\n" +
-            $"Описание: {module.Info.Description}\n" +
-            $"{module.Info.TypeName}";
-
-        Icon IHasIcon.Icon => module.Info.ModuleColor.Name switch
-        {
-            nameof(Color.Black) => Icon.BlackModule,
-            nameof(Color.Blue) => Icon.BlueModule,
-            nameof(Color.Gray) => Icon.GrayModule,
-            nameof(Color.Green) => Icon.GreenModule,
-            nameof(Color.Lime) => Icon.LimeModule,
-            nameof(Color.Orange) => Icon.OrangeModule,
-            nameof(Color.Red) => Icon.RedModule,
-            nameof(Color.Violet) => Icon.VioletModule,
-            nameof(Color.Yellow) => Icon.YellowModule,
-            "0" => Icon.EmptyModule,
-            _ => Icon.None
-        };
+        protected override string ToolTipName =>
+            IOModule.Function?.VisibleName ?? Name;
     }
 }
--- src/IO/ViewModel/Module.cs
+++ src/IO/ViewModel/Module.cs
@@ -1,13 +1,9 @@
-using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IO.ViewModel
 {
-    public class Module : IModule, IExpandable, IHasIcon, IToolTip
+    public class Module : ModuleViewItem, IModule, IExpandable
     {
         private readonly INode owner;
 
@@ -15,10 +11,9 @@ namespace IO.ViewModel
 
 
         public Module(IIOModule module, INode owner)
+            : base(module)
         {
             this.owner = owner;
-
-            IOModule = module;
 
             binds.AddRange(module.Info.ChannelClamps
                 .OrderBy(c => c)
@@ -27,33 +22,7 @@ namespace IO.ViewModel
 
         public IEnumerable<IViewItem> Items => binds;
 
-        public string Name => IOModule.Name;
-
-        public string Description => IOModule.Info.Description;
-
-        string IToolTip.Description => 
-            $"Артикул: {IOModule.Info.Name}\n" +
-            $"Описание: {IOModule.Info.Description}\n" +
-            $"{IOModule.Info.TypeName}";
-
-        public IIOModule IOModule { get; private set; }
-
         public IIONode IONode => owner.IONode;
-
-        Icon IHasIcon.Icon => IOModule.Info.ModuleColor.Name switch
-        {
-            nameof(Color.Black) => Icon.BlackModule,
-            nameof(Color.Blue) => Icon.BlueModule,
-            nameof(Color.Gray) => Icon.GrayModule,
-            nameof(Color.Green) => Icon.GreenModule,
-            nameof(Color.Lime) => Icon.LimeModule,
-            nameof(Color.Orange) => Icon.OrangeModule,
-            nameof(Color.Red) => Icon.RedModule,
-            nameof(Color.Violet) => Icon.VioletModule,
-            nameof(Color.Yellow) => Icon.YellowModule,
-            "0" => Icon.EmptyModule,
-            _ => Icon.None
-        };
 
         public bool Expanded 
         { 
