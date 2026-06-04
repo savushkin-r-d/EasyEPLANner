3615538b9d900e99eb30a9229fda65fdab313633
KirillGutyrchik <gutyrcikkirill@gmail.com>
2026-06-04 15:51:07 +0300

    change go to fas

--- src/StaticHelper/EplanNavigateHelper.cs
+++ src/StaticHelper/EplanNavigateHelper.cs
@@ -1,4 +1,6 @@
+using Eplan.EplApi.ApplicationFramework;
 using Eplan.EplApi.DataModel;
+using Eplan.EplApi.HEServices;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Windows.Forms;
@@ -6,19 +8,28 @@ using System.Windows.Forms;
 namespace StaticHelper
 {
     /// <summary>
-    /// Переход на страницу ФСА (функциональная схема автоматизации) по функции EPLAN.
+    /// Показ функции на схеме EPLAN: открытие страницы, выделение и приближение к объекту.
     /// </summary>
     [ExcludeFromCodeCoverage]
     public static class EplanNavigateHelper
     {
+        private static readonly string[] ZoomToSelectionActionNames =
+        {
+            "XGedZoomMarkedObjectsAction",
+            "XGedZoomMarkedAction",
+            "gedZoomMarkedObjects",
+        };
+
         public static void OpenFunctionPage(Function function)
         {
             var project = function.Page?.Project
                 ?? throw new InvalidOperationException(
                     "Не найден проект страницы выбранного объекта.");
 
-            var edit = new Eplan.EplApi.HEServices.Edit();
-            edit.OpenPageWithName(project.ProjectLinkFilePath, function.Page.Name);
+            var edit = new Edit();
+            OpenPageAndSelect(edit, function, project.ProjectLinkFilePath);
+            edit.SetFocusToGED();
+            TryZoomToSelection();
         }
 
         public static void OpenFunctionPageWithError(Function function)
@@ -46,6 +57,52 @@ namespace StaticHelper
 
             function = functionWrapper.Function;
             return true;
+        }
+
+        private static void OpenPageAndSelect(Edit edit, Function function, string projectPath)
+        {
+            try
+            {
+                edit.OpenPageWithPlacement(function);
+                return;
+            }
+            catch
+            {
+                // fallback below
+            }
+
+            if (!string.IsNullOrEmpty(function.VisibleName))
+            {
+                edit.OpenPageWithNameAndDeviceName(
+                    projectPath, function.Page.Name, function.VisibleName);
+                return;
+            }
+
+            edit.OpenPageWithName(projectPath, function.Page.Name);
+        }
+
+        private static void TryZoomToSelection()
+        {
+            try
+            {
+                var cli = new CommandLineInterpreter();
+                foreach (var actionName in ZoomToSelectionActionNames)
+                {
+                    try
+                    {
+                        if (cli.Execute(actionName))
+                            return;
+                    }
+                    catch
+                    {
+                        // пробуем следующее имя внутреннего действия EPLAN
+                    }
+                }
+            }
+            catch
+            {
+                // приближение необязательно: страница уже открыта и объект выделен
+            }
         }
     }
 }
