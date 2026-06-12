91dc28652bff525ef779a81c45688eb01ae365ff
KirillGutyrchik <gutyrcikkirill@gmail.com>
2026-06-12 15:11:56 +0300

    test device tree items

---
+++ EasyEplanner.Tests/Device.Test/ViewModel.Test/DevicesTreeItems.Test.cs
@@ -0,0 +1,241 @@
+using EasyEPlanner.Devices.ViewModel;
+using EasyEPlanner.Devices.ViewModel.ViewInterface;
+using EplanDevice;
+using Moq;
+using NUnit.Framework;
+using StaticHelper;
+using System.Linq;
+using System.Reflection;
+
+namespace EasyEPlanner.Devices.Tests
+{
+    public class DevicesTreeItemsTest
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            ResetDeviceManager();
+        }
+
+        [Test]
+        public void TypeGroupNode_UpdateHeader_IncludesDeviceCount()
+        {
+            var context = new DevicesViewModel(null);
+            var typeNode = new DevicesTypeGroupNode(context,
+                (FilterableViewItemBase)context.Root, "DO", DeviceType.DO);
+
+            typeNode.IncrementCount();
+            typeNode.IncrementCount();
+            typeNode.UpdateHeader();
+
+            Assert.AreEqual("DO (2)", typeNode.Name);
+            Assert.AreEqual(DevicesIcon.Type, typeNode.Icon);
+        }
+
+        [Test]
+        public void ObjectGroupNode_UpdateHeader_AndSearchableText()
+        {
+            var context = new DevicesViewModel(null);
+            var objectNode = new DevicesObjectGroupNode(context,
+                (FilterableViewItemBase)context.Root, "TANK2", "TANK2");
+
+            objectNode.IncrementCount();
+            objectNode.UpdateHeader();
+
+            Assert.AreEqual("TANK2 (1)", objectNode.Name);
+            StringAssert.Contains("TANK2", objectNode.GetSearchableText());
+            Assert.AreEqual(DevicesIcon.Object, objectNode.Icon);
+        }
+
+        [Test]
+        public void DeviceNode_BuildGroups_ChildrenHaveGroupParent()
+        {
+            var device = CreateTankDoDevice();
+            var context = new DevicesViewModel(null);
+            var deviceNode = new DevicesDeviceNode(context,
+                (FilterableViewItemBase)context.Root, device, device.Name);
+
+            var groupNames = deviceNode.Items.OfType<DevicesGroupNode>()
+                .Select(g => g.Name).ToArray();
+            CollectionAssert.AreEquivalent(
+                new[] { "Данные", "Каналы" }, groupNames);
+
+            var dataGroup = deviceNode.Items.OfType<DevicesGroupNode>()
+                .Single(g => g.Name == "Данные");
+            var subtypeItem = dataGroup.Items.OfType<DevicesSubtypeItem>().Single();
+            Assert.AreSame(dataGroup, subtypeItem.ParentItem);
+
+            var channelsGroup = deviceNode.Items.OfType<DevicesGroupNode>()
+                .Single(g => g.Name == "Каналы");
+            var channelItem = channelsGroup.Items.OfType<DevicesChannelItem>().Single();
+            Assert.AreSame(channelsGroup, channelItem.ParentItem);
+            Assert.AreSame(device, channelItem.Device);
+        }
+
+        [Test]
+        public void TreeBuilder_TypeThenObject_GroupsByTypeAndObject()
+        {
+            var device = CreateTankDoDevice();
+            var context = CreateContextWithDevices(device);
+
+            var root = (DevicesRoot)context.Root;
+            Assert.AreEqual("Устройства проекта (1)", root.Name);
+
+            var typeNode = root.Items.OfType<DevicesTypeGroupNode>().Single();
+            Assert.AreEqual("DO (1)", typeNode.Name);
+
+            var objectNode = typeNode.Items.OfType<DevicesObjectGroupNode>().Single();
+            Assert.AreEqual("TANK2 (1)", objectNode.Name);
+
+            var deviceNode = objectNode.Items.OfType<DevicesDeviceNode>().Single();
+            Assert.AreSame(device, deviceNode.Device);
+        }
+
+        [Test]
+        public void TreeBuilder_TypeThenObject_PlacesDeviceWithoutObjectUnderType()
+        {
+            var device = new DO("STANDALONE1", "+STANDALONE1", "desc", 1, "", 0);
+            device.SetSubType("DO");
+            var context = CreateContextWithDevices(device);
+
+            var root = (DevicesRoot)context.Root;
+            var typeNode = root.Items.OfType<DevicesTypeGroupNode>().Single();
+            var deviceNode = typeNode.Items.OfType<DevicesDeviceNode>().Single();
+
+            Assert.IsEmpty(typeNode.Items.OfType<DevicesObjectGroupNode>());
+            Assert.AreSame(device, deviceNode.Device);
+        }
+
+        [Test]
+        public void TreeBuilder_TypeThenObject_OmitsEmptyTypeGroups()
+        {
+            var device = CreateTankDoDevice();
+            var context = CreateContextWithDevices(device);
+
+            var root = (DevicesRoot)context.Root;
+            var typeKeys = root.Items.OfType<DevicesTypeGroupNode>()
+                .Select(n => n.TypeKey).ToArray();
+
+            CollectionAssert.AreEqual(new[] { "DO" }, typeKeys);
+        }
+
+        [Test]
+        public void TreeBuilder_ObjectThenType_GroupsByObjectAndType()
+        {
+            var device = CreateTankDoDevice();
+            var context = CreateContextWithDevices(device);
+            context.GroupingMode = DevicesGroupingMode.ObjectThenType;
+            context.RebuildTree();
+
+            var root = (DevicesRoot)context.Root;
+            var objectNode = root.Items.OfType<DevicesObjectGroupNode>().Single();
+            Assert.AreEqual("TANK2 (1)", objectNode.Name);
+
+            var typeNode = objectNode.Items.OfType<DevicesTypeGroupNode>().Single();
+            Assert.AreEqual("DO (1)", typeNode.Name);
+
+            var deviceNode = typeNode.Items.OfType<DevicesDeviceNode>().Single();
+            Assert.AreSame(device, deviceNode.Device);
+        }
+
+        [Test]
+        public void TreeBuilder_ResolvesVirtSubtypeUnderDedicatedTypeNode()
+        {
+            var device = new DO("VIRT1", "+VIRT1", "desc", 1, "TANK", 1);
+            device.SetSubType("DO_VIRT");
+            var context = CreateContextWithDevices(device);
+
+            var root = (DevicesRoot)context.Root;
+            var typeNode = root.Items.OfType<DevicesTypeGroupNode>().Single();
+            Assert.AreEqual("DO_VIRT (1)", typeNode.Name);
+        }
+
+        [Test]
+        public void FilterableViewItemBase_Filter_FindsDeviceByEplanName()
+        {
+            var device = CreateTankDoDevice();
+            var context = CreateContextWithDevices(device);
+            var root = (DevicesRoot)context.Root;
+            root.ResetFilter();
+
+            Assert.IsTrue(root.Filter("+TANK2-DO1", hideEmptyItems: false));
+            Assert.IsTrue(context.SearchContext.FoundItems
+                .OfType<DevicesDeviceNode>().Any());
+        }
+
+        [Test]
+        public void FilterableViewItemBase_ResetFilter_AllowsNewSearch()
+        {
+            var device = CreateTankDoDevice();
+            var context = CreateContextWithDevices(device);
+
+            var root = (DevicesRoot)context.Root;
+            Assert.IsTrue(root.Filter("+TANK2-DO1", hideEmptyItems: false));
+            root.ResetFilter();
+
+            Assert.IsFalse(root.Filter("ZZZNOMATCH", hideEmptyItems: false));
+        }
+
+        [Test]
+        public void DevicesPropertyItem_SetValue_UpdatesDescriptionAndEplanFunction()
+        {
+            var device = CreateTankDoDevice();
+            var function = new Mock<IEplanFunction>();
+            device.Function = function.Object;
+            device.Properties["TestProp"] = "old";
+
+            var context = new DevicesViewModel(null);
+            var deviceNode = new DevicesDeviceNode(context,
+                (FilterableViewItemBase)context.Root, device, device.Name);
+            var item = new DevicesPropertyItem(context, deviceNode, device,
+                "TestProp", "old");
+
+            Assert.IsTrue(item.SetValue("new"));
+            Assert.AreEqual("new", item.Description);
+            Assert.AreEqual("new", device.Properties["TestProp"]);
+            function.VerifySet(f => f.Properties = It.IsAny<string>());
+        }
+
+        [Test]
+        public void DevicesRuntimeParameterItem_SetValue_ParsesInteger()
+        {
+            var device = CreateTankDoDevice();
+            device.Function = Mock.Of<IEplanFunction>();
+            device.SetRuntimeParameter("RParam", 5);
+
+            var context = new DevicesViewModel(null);
+            var deviceNode = new DevicesDeviceNode(context,
+                (FilterableViewItemBase)context.Root, device, device.Name);
+            var item = new DevicesRuntimeParameterItem(context, deviceNode, device,
+                "RParam", "5");
+
+            Assert.IsFalse(item.SetValue("not-a-number"));
+            Assert.IsTrue(item.SetValue("10"));
+            Assert.AreEqual("10", item.Description);
+        }
+
+        private static DO CreateTankDoDevice()
+        {
+            var device = new DO("TANK2DO1", "+TANK2-DO1", "desc", 1, "TANK", 2);
+            device.SetSubType("DO");
+            return device;
+        }
+
+        private static DevicesViewModel CreateContextWithDevices(params IODevice[] devices)
+        {
+            var manager = DeviceManager.GetInstance();
+            manager.Devices.Clear();
+            foreach (var device in devices)
+                manager.Devices.Add(device);
+
+            return new DevicesViewModel(manager);
+        }
+
+        private static void ResetDeviceManager()
+        {
+            var instance = typeof(DeviceManager).GetField("instance",
+                BindingFlags.NonPublic | BindingFlags.Static);
+            instance.SetValue(null, null);
+        }
+    }
+}
