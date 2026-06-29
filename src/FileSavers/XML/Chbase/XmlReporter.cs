43e4bc264fc24f014cf5dcb34f285edcc3af7294
KirillGutyrchik <gutyrcikkirill@gmail.com>
2026-06-29 12:58:10 +0300

    fix auto path

--- src/FileSavers/XML/Chbase/XmlReporter.cs
+++ src/FileSavers/XML/Chbase/XmlReporter.cs
@@ -636,7 +636,7 @@ namespace EasyEPlanner
         public void AutomaticExportNewChannelBaseCombineTags(
             string projectDirPath, string projectName)
         {
-            string path = Path.Combine(projectDirPath, "chbase");
+            string path = Path.Combine(projectDirPath, "DOC");
             Directory.CreateDirectory(path);
             path = Path.Combine(path, $"{projectName}.cdbx");
             SaveAsXML(path, true, true);
