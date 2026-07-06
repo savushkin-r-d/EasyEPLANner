6000ea22b7c03d71bc046ba527b662036430badf
KirillGutyrchik <gutyrcikkirill@gmail.com>
2026-07-06 08:29:05 +0300

    [Interproject Exchange] Filter extension: virtual devices (#1816)
    Fixes #1815.
    
    ```ChangeLog
    В межпроектном обмене сигналами расширен фильтр: теперь все виртуальные устройства можно выбрать отдельно;
    ```

--- EasyEplanner.Tests/InterprojectExchange.Test/TestData/mock_script.lua
+++ EasyEplanner.Tests/InterprojectExchange.Test/TestData/mock_script.lua
@@ -1,4 +1,17 @@
-﻿function init_io_file (projecrt_name)
-    -- do nothing
-    return 0
+﻿init_io_file = function(projectName)
+    if (devices == nil or nodes == nil or PAC_name == "") then
+        return
+    end
+
+    local model = CreateMainModel()
+    model.ProjectName = PAC_name
+
+    for key, value in pairs(devices) do
+        local name = value.name or ""
+        local descr = value.descr or ""
+        local dtype = value.dtype or -1
+        local subtype = value.subtype or 0
+        model:AddDeviceData(name, descr, dtype, subtype)
+    end
+    model:SortDeviceData()
 end
